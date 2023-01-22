6a9556e12163f8e2c9137295fe1bbe8d07bdb490
AntonZgriski <anton.zgirski@gmail.com>
2023-01-22 22:20:13 +0300

    Done

---
+++ Lab9/Lab9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using System.Xml.Serialization;
+using System.Text;
+
+namespace Lab9
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var squad = new Squad("People", "100","World");
+      var squadJson = JsonConvert.SerializeObject(squad);
+      string pars;
+      IFormatter formatter = new BinaryFormatter();
+      
+      using (Stream stream = new FileStream("Squad.json", FileMode.Create, FileAccess.Write, FileShare.None))
+      {
+        formatter.Serialize(stream, squadJson);
+      }
+
+      using (Stream stream = new FileStream("Squad.json", FileMode.Open, FileAccess.Read, FileShare.None))
+      {
+        pars = formatter.Deserialize(stream).ToString();
+      }
+
+      var squadDis = new Squad(pars);
+
+      using (Stream stream = new FileStream("Squad.bin", FileMode.Create, FileAccess.Write, FileShare.None))
+      {
+        formatter.Serialize(stream, squadDis);
+      }
+
+      XmlSerializer xml = new XmlSerializer(typeof(Squad));
+      using (Stream stream = new FileStream("Squad.xml", FileMode.Create))
+      {
+        xml.Serialize(stream, squadDis);
+      }
+
+    }
+  }
+}
---
+++ Lab9/Lab9/Squad.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab9
+{
+  [Serializable]
+  public class Squad
+  {
+    public string nameSquad { get; set; }
+    public string countSquad { get; set; }
+    public string placeSquad { get; set; }
+    public Squad()
+    {
+    }
+    public Squad(string name, string count, string place)
+    {
+      this.nameSquad = name;
+      this.countSquad = count;
+      this.placeSquad = place;
+    }
+
+    public Squad(string pars)
+    {
+      pars = pars.Trim(new char[] { '{', '}' });
+      
+      string[] one = pars.Split(',', ':');
+
+      StringBuilder str = new StringBuilder();
+
+      for (int i = 0; i < one.Length; i++)
+      {
+        if (i % 2 != 0)
+        {
+          str.Append(one[i].Trim('"'));
+          if (i != one.Length - 1) str.Append(",");
+        }
+      }
+      
+      string finish = str.ToString();
+      string[] fin = finish.Split(',');
+
+      this.nameSquad = fin[0];
+      this.countSquad = fin[1];
+      this.placeSquad = fin[2];
+    }
+  }
+}
